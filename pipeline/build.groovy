e7c50468669419968d5cd4ac43b842fedfcf66d5
hardikhard28 <63140787+hardikhard28@users.noreply.github.com>
2023-06-08 08:24:42 +0530

    pushing

--- pipeline/build.groovy
+++ pipeline/build.groovy
@@ -9,26 +9,7 @@ pipeline {
                     checkout([
                         $class: 'GitSCM',
                         branches: [[name: '*/dev']], // Specify the branch you want to checkout
-                        userRemoteConfigs: [[url: 'pipeline {
-    agent any
-
-    stages {
-        stage('Checkout') {
-            steps {
-                script {
-                    // Use the checkout step to clone the Git repository
-                    checkout([
-                        $class: 'GitSCM',
-                        branches: [[name: '*/dev']], // Specify the branch you want to checkout
                         userRemoteConfigs: [[url: 'https://github.com/hardikhard28/Hello-jenkins-android.git']] // Specify your Git repository URL
-                    ])
-                }
-            }
-        }
-        
-        
-    }
-}']] // Specify your Git repository URL
                     ])
                 }
             }
